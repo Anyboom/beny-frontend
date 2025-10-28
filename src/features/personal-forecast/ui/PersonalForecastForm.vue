95c35b026ff862a2ee9a62d76a77d0d701135db3
Даниил <danilka.volkov.02@mail.ru>
2025-10-29 00:25:30 +0300

    feat: integrate tanstacks forms

--- src/features/personal-forecast/ui/PersonalForecastForm.vue
+++ src/features/personal-forecast/ui/PersonalForecastForm.vue
@@ -1,124 +1,156 @@
 <script setup lang="ts">
-  import { useId } from "#imports";
   import { AppButton } from "~/shared/ui/AppButton";
-  import { useForm } from "~/shared/composables/use-form";
+  import { revalidateLogic, useForm } from "@tanstack/vue-form";
   import { personalForecastSchema } from "~/features/personal-forecast/model/personal-forecast.schema";
   import { createForecastApi } from "~/features/personal-forecast/api/create-forecast.api";
 
-  const ids = {
-    name: useId(),
-    communicationMethod: useId(),
-    coefficient: useId(),
-    message: useId(),
-  };
-
-  const { handleSubmit, values, errors, reset } = useForm({
-    schema: personalForecastSchema,
-    initialValues: {
+  const form = useForm({
+    defaultValues: {
       name: "",
       coefficient: "",
       communicationMethod: "",
       message: "",
     },
+    validationLogic: revalidateLogic(),
+    validators: {
+      onDynamic: personalForecastSchema,
+    },
+    onSubmit: async ({ value, formApi }) => {
+      try {
+        await createForecastApi(value);
+      } finally {
+        formApi.reset();
+      }
+    },
   });
-
-  async function onSubmit() {
-    try {
-      await createForecastApi(values.value);
-    } finally {
-      reset();
-    }
-  }
 </script>
 
 <template>
   <form
     class="personal-forecast-form"
-    @submit.prevent="handleSubmit(onSubmit)"
+    @submit="
+      (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        form.handleSubmit();
+      }
+    "
   >
-    <div class="personal-forecast-form__group">
-      <label
-        class="personal-forecast-form__label"
-        :for="ids.name"
-      >
-        Имя
-      </label>
-      <input
-        :id="ids.name"
-        v-model="values.name"
-        class="personal-forecast-form__input"
-        type="text"
-      />
-      <span
-        v-show="errors.name"
-        class="personal-forecast-form__error"
-      >
-        {{ errors.name }}
-      </span>
-    </div>
-    <div class="personal-forecast-form__group">
-      <label
-        class="personal-forecast-form__label"
-        :for="ids.communicationMethod"
-      >
-        Способ связи
-      </label>
-      <input
-        :id="ids.communicationMethod"
-        v-model="values.communicationMethod"
-        class="personal-forecast-form__input"
-        type="text"
-      />
-      <span
-        v-show="errors.communicationMethod"
-        class="personal-forecast-form__error"
-      >
-        {{ errors.communicationMethod }}
-      </span>
-    </div>
-    <div class="personal-forecast-form__group">
-      <label
-        class="personal-forecast-form__label"
-        :for="ids.coefficient"
-      >
-        Желаемый коэффициент
-      </label>
-      <input
-        :id="ids.coefficient"
-        v-model="values.coefficient"
-        class="personal-forecast-form__input"
-        type="text"
-      />
-      <span
-        v-show="errors.coefficient"
-        class="personal-forecast-form__error"
-      >
-        {{ errors.coefficient }}
-      </span>
-    </div>
-    <div class="personal-forecast-form__group">
-      <label
-        class="personal-forecast-form__label"
-        :for="ids.message"
-      >
-        Комментарий
-      </label>
-      <textarea
-        :id="ids.message"
-        v-model="values.message"
-        class="personal-forecast-form__textarea"
-        rows="15"
-      ></textarea>
-      <span
-        v-show="errors.message"
-        class="personal-forecast-form__error"
-      >
-        {{ errors.message }}
-      </span>
-    </div>
-    <div class="personal-forecast-form__buttons">
-      <AppButton> Отправить </AppButton>
-    </div>
+    <form.Field name="name">
+      <template #default="{ field, state }">
+        <div class="personal-forecast-form__group">
+          <label
+            class="personal-forecast-form__label"
+            :for="field.name"
+          >
+            Имя
+          </label>
+          <input
+            :id="field.name"
+            class="personal-forecast-form__input"
+            type="text"
+            :name="field.name"
+            :value="state.value"
+            @input="(e) => field.handleChange((e.target as HTMLInputElement).value)"
+            @blur="field.handleBlur"
+          />
+          <span
+            v-if="!field.state.meta.isValid && field?.state?.meta?.errors?.at(0)"
+            class="personal-forecast-form__error"
+          >
+            {{ field.state.meta.errors.at(0)?.message }}
+          </span>
+        </div>
+      </template>
+    </form.Field>
+    <form.Field name="communicationMethod">
+      <template #default="{ field, state }">
+        <div class="personal-forecast-form__group">
+          <label
+            class="personal-forecast-form__label"
+            :for="field.name"
+          >
+            Способ связи
+          </label>
+          <input
+            :id="field.name"
+            class="personal-forecast-form__input"
+            type="text"
+            :value="state.value"
+            :name="field.name"
+            @input="(e) => field.handleChange((e.target as HTMLInputElement).value)"
+            @blur="field.handleBlur"
+          />
+          <span
+            v-if="!field.state.meta.isValid && field?.state?.meta?.errors?.at(0)"
+            class="personal-forecast-form__error"
+          >
+            {{ field.state.meta.errors.at(0)?.message }}
+          </span>
+        </div>
+      </template>
+    </form.Field>
+    <form.Field name="coefficient">
+      <template #default="{ field, state }">
+        <div class="personal-forecast-form__group">
+          <label
+            class="personal-forecast-form__label"
+            :for="field.name"
+          >
+            Желаемый коэффициент
+          </label>
+          <input
+            :id="field.name"
+            class="personal-forecast-form__input"
+            type="text"
+            :value="state.value"
+            :name="field.name"
+            @input="(e) => field.handleChange((e.target as HTMLInputElement).value)"
+            @blur="field.handleBlur"
+          />
+          <span
+            v-if="!field.state.meta.isValid && field?.state?.meta?.errors?.at(0)"
+            class="personal-forecast-form__error"
+          >
+            {{ field.state.meta.errors.at(0)?.message }}
+          </span>
+        </div>
+      </template>
+    </form.Field>
+    <form.Field name="message">
+      <template #default="{ field, state }">
+        <div class="personal-forecast-form__group">
+          <label
+            class="personal-forecast-form__label"
+            :for="field.name"
+          >
+            Комментарий
+          </label>
+          <textarea
+            :id="field.name"
+            class="personal-forecast-form__textarea"
+            rows="15"
+            :value="state.value"
+            :name="field.name"
+            @input="(e) => field.handleChange((e.target as HTMLInputElement).value)"
+            @blur="field.handleBlur"
+          ></textarea>
+          <span
+            v-if="!field.state.meta.isValid && field?.state?.meta?.errors?.at(0)"
+            class="personal-forecast-form__error"
+          >
+            {{ field.state.meta.errors.at(0)?.message }}
+          </span>
+        </div>
+      </template>
+    </form.Field>
+    <form.Subscribe>
+      <template #default="{ isSubmitting }">
+        <div class="personal-forecast-form__buttons">
+          <AppButton :disabled="isSubmitting"> Отправить </AppButton>
+        </div>
+      </template>
+    </form.Subscribe>
   </form>
 </template>
 
