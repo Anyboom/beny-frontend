ec78fcf3ae4bfeea7c7858866894a93e1f1c1c37
Даниил <danilka.volkov.02@mail.ru>
2025-10-31 22:39:16 +0300

    feat: add AppSkeleton and skeleton for BetCard

---
+++ src/entities/bet/ui/BetCardSkeleton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { AppSkeleton } from "~/shared/ui/AppSkeleton";
+</script>
+
+<template>
+  <AppSkeleton
+    height="243px"
+    border-radius="16px"
+  />
+</template>
---
+++ src/shared/ui/AppSkeleton/ui/AppSkeleton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  interface AppSkeletonProps {
+    width?: string;
+    height?: string;
+    borderRadius?: string;
+  }
+
+  const { height = "100%", width = "100%", borderRadius = undefined } = defineProps<AppSkeletonProps>();
+</script>
+
+<template>
+  <div
+    class="app-skeleton"
+    :style="{
+      width: width,
+      height: height,
+      borderRadius: borderRadius,
+    }"
+  ></div>
+</template>
+
+<style scoped>
+  .app-skeleton {
+    background-color: #e0e0e0;
+    position: relative;
+    overflow: hidden;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.4), transparent);
+      animation: shimmer 1.5s infinite;
+    }
+  }
+
+  @keyframes shimmer {
+    0% {
+      left: -100%;
+    }
+    100% {
+      left: 100%;
+    }
+  }
+</style>
