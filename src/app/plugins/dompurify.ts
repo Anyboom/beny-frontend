ba06e899af25555f43579273007d76c6eb4a98f4
Даниил <danilka.volkov.02@mail.ru>
2025-11-03 03:58:55 +0300

    fix: change package for dompurify

---
+++ src/app/plugins/dompurify.ts
@@ -0,0 +1,6 @@
+import VueDOMPurifyHTML from "vue-dompurify-html";
+import { defineNuxtPlugin } from "#app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueDOMPurifyHTML);
+});
