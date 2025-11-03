f8f21d0ad2458dcd38c7a204d0696b0b47a0cd92
Даниил <danilka.volkov.02@mail.ru>
2025-11-04 01:08:50 +0300

    chore: change eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,14 @@ export default defineConfig([
     rules: {
       "vue/multi-word-component-names": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_",
+        },
+      ],
       "vue/singleline-html-element-content-newline": "off",
       "vue/no-v-html": "off",
       "vue/html-self-closing": [
