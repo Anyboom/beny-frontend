7cd005c0835fc8c606396ea20666981bdba95a6b
Даниил <danilka.volkov.02@mail.ru>
2026-01-14 03:03:43 +0300

    feat: add styles for error page

---
+++ src/app/layouts/error.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="error-layout">
+    <div class="error-layout__wrapper">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @use "~/shared/assets/styles/variables" as *;
+  @use "~/shared/assets/styles/mixins" as *;
+
+  body {
+    :has(.error-layout) {
+      background: $color-accent;
+    }
+  }
+
+  .error-layout {
+    height: 100dvh;
+    padding: $spacing-2;
+    background: $color-accent;
+
+    &__wrapper {
+      background: $color-whisper;
+      border-radius: $spacing-3;
+      padding: 0 $spacing-3;
+      overflow: auto;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      @include view-port-sm {
+        border-radius: 0;
+        padding: 0 $spacing-1;
+      }
+    }
+
+    @include view-port-sm {
+      padding: 0;
+    }
+  }
+</style>
--- src/error.vue
+++ src/error.vue
@@ -1,17 +1,74 @@
 <script setup lang="ts">
   import type { NuxtError } from "#app";
+  import { AppButton } from "~/shared/ui/AppButton";
+  import { computed } from "#imports";
 
   interface Props {
     error: NuxtError;
   }
 
-  defineProps<Props>();
+  type Error = {
+    title: string;
+    caption: string;
+  };
+
+  const errors: Record<string, Error> = {
+    404: {
+      title: "Страница не найдена",
+      caption:
+        "К сожалению, страница, которую вы ищете, не найдена. Возможно, она была перемещена, удалена или ссылка ведет не туда.",
+    },
+    "default": {
+      title: "Что-то пошло не так...",
+      caption: "Произошла непредвиденная ошибка. Не волнуйтесь, это не ваша вина.",
+    },
+  };
+
+  const { error } = defineProps<Props>();
+
+  const currentError = computed(() => {
+    if (errors?.[error.statusCode]) {
+      return errors[error.statusCode] as Error;
+    }
+
+    return errors.default as Error;
+  });
 </script>
 
 <template>
-  {{ error }}
-  <span>{{ error.statusCode }}</span>
-  <span>Ошибка</span>
+  <NuxtLayout name="error">
+    <div class="error">
+      <h1 class="error__title">
+        {{ currentError.title }}
+      </h1>
+      <span class="error__caption">
+        {{ currentError.caption }}
+      </span>
+      <AppButton class="error__button">Вернуться на главную</AppButton>
+    </div>
+  </NuxtLayout>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+  @use "@/shared/assets/styles/typography" as *;
+  @use "@/shared/assets/styles/variables" as *;
+
+  .error {
+    max-width: $spacing-16 * 4;
+    display: flex;
+    flex-direction: column;
+    gap: $spacing-2;
+
+    &__title {
+      @include apply-text("heading-1");
+    }
+
+    &__caption {
+      @include apply-text("body");
+    }
+
+    &__button {
+      width: fit-content;
+    }
+  }
+</style>
