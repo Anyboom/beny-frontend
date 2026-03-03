751033c2ae6a88542fd80b6ce54a7b8929dff19c
Даниил <danilka.volkov.02@mail.ru>
2026-03-03 20:02:58 +0300

    Revert "docs: add histoire"
    This reverts commit 476673e4ade4f4512f931b1a20140dbcd9b63b49.

--- histoire.config.ts
+++
@@ -1,7 +0,0 @@
-import { HstNuxt } from "@histoire/plugin-nuxt";
-import { defineConfig } from "histoire";
-import { HstVue } from "@histoire/plugin-vue";
-
-export default defineConfig({
-  plugins: [HstNuxt(), HstVue()],
-});
